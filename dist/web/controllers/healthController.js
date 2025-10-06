04e23cba207b838f7a5d0b53bf79a447ddc86a9b
Mike Hsieh <hsiehdog@hotmail.com>
2025-10-06 11:11:28 -0700

    Remove dist folder from repo

--- dist/web/controllers/healthController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HealthController = void 0;
-class HealthController {
-    static getHealth(req, res) {
-        res.json({ status: "ok" });
-    }
-}
-exports.HealthController = HealthController;
